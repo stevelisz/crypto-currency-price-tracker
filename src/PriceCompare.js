c6401d53ca083deb116a4831eef8f33c4a4ee648
shuaizhenli <li.shuai@husky.neu.edu>
2021-10-22 02:29:18 -0700

    Update PriceCompare.js

--- src/PriceCompare.js
+++ src/PriceCompare.js
@@ -5,6 +5,8 @@ import React, { useState, useEffect } from "react";
 const PriceCompare = () => {
   const [WhereBTC, setBTCName] = useState(null);
   const [WhereETH, setETHName] = useState(null);
+  const [WhereSellBTC, setsellBTC] = useState(null);
+  const [WhereSellETH, setsellETH] = useState(null);
   const urls = ["http://api.coinlayer.com/api/live?access_key=7909ef414546b1a00cce0cecf7adcc9a&symbols=%20BTC,ETH", "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=BTC,USD","https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=ETH,USD"]
 
   Promise.all([
@@ -26,21 +28,25 @@ const PriceCompare = () => {
     //console.log(data[3]);
     if(parseFloat(data[0].data.amount) < parseFloat(data[2].USD)){
       setBTCName('Coinbase')
+      setsellBTC('CryptoCompare')
     }
     if(parseFloat(data[0].data.amount) > parseFloat(data[2].USD)){
       setBTCName('CryptoCompare')
+      setsellBTC('Coinbase')
     }
     if(parseFloat(data[0].data.amount) == parseFloat(data[2].USD)){
-      setBTCName('You can purchase it on both platforms since the price is the same.')
+      setBTCName('You can purchase or sell on both platforms since the price is the same.')
     }
     if(parseFloat(data[1].data.rates.USD) < parseFloat(data[3].USD)){
       setETHName('Coinbase')
+      setsellETH('CryptoCompare')
     }
     if(parseFloat(data[1].data.rates.USD) > parseFloat(data[3].USD)){
       setETHName('CryptoCompare')
+      setsellETH('Coinbase')
     }
     if(parseFloat(data[1].data.rates.USD) == parseFloat(data[3].USD)){
-      setETHName('You can purchase it on both platforms since the price is the same.')
+      setETHName('You can purchase or sell on both platforms since the price is the same.')
     }
     
   }).catch(function (error) {
@@ -54,8 +60,12 @@ const PriceCompare = () => {
     <div className="btc">
       <h3>Where to buy BTC at the moment?</h3>
       <span className="btc-where">{WhereBTC}</span> 
+      <h3>Where to sell BTC at the moment?</h3>
+      <span className="btc-where">{WhereSellBTC}</span> 
       <h3>Where to buy ETH at the moment?</h3>
-      <span className="eth-where">{WhereETH}</span>    
+      <span className="eth-where">{WhereETH}</span>
+      <h3>Where to buy ETH at the moment?</h3>
+      <span className="eth-where">{WhereSellETH}</span>     
     </div>
     
   );
