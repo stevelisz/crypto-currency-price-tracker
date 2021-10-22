cd94b85ed46581c40e2895835fcaabad03c9db4b
shuaizhenli <li.shuai@husky.neu.edu>
2021-10-22 02:54:22 -0700

    logo added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import CoinLayerBtc from './CoinLayerBtc';
 import CoinLayerEth from './CoinLayerEth';
 import PriceCompare from './PriceCompare';
 import './App.css';
-
+import btclogo from "./bitcoin-logo.png";
+import ethlogo from "./eth-logo.png";
 function App() {
   function refreshPage() {
     window.location.reload();
@@ -12,15 +13,22 @@ function App() {
   
   return (
     <div className="App">
+      <div>
+      <img className="photo" src={btclogo} alt="btc logo"/>
+      </div>
       <CryptocompareBtc />
       <CoinLayerBtc />
+      <div>
+      <img className="photo" src={ethlogo} alt="eth logo"/>
+      </div>
       <CryptocompareEth />
       <CoinLayerEth />
+      {' '}
       <PriceCompare />
       <div>
         
       </div>
-      <button color="black" onClick={refreshPage}>Refresh</button>
+      <button onClick={refreshPage}>Refresh</button>
     </div>
   );
 }
